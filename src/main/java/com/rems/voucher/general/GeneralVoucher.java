e4739cde67d6faf455851b17ea3d74346df171b7
bcssp10 <znmansoor@gmail.com>
2017-09-07 17:42:11 +0500

    [ Trail Balance ] Bugs

--- src/main/java/com/rems/voucher/general/GeneralVoucher.java
+++ src/main/java/com/rems/voucher/general/GeneralVoucher.java
@@ -37,39 +37,42 @@ import com.rems.party.Party;
 		 */
 })
 @NamedNativeQueries({
-		@NamedNativeQuery(name = "GeneralVoucher.findTrialBalance", query = "select concat('P-',LPAD(party_Id, 2, '0')) id,Account, null as debit,sum(credit) as credit from ( \r\n" + 
-				"select cash_paid_to as party_Id,(select name from party where cash_paid_to=party_id) Account,SUM(amount) credit FROM `general_voucher` where cash_paid_by=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"union all\r\n" + 
-				"select party_Id, (select name from party p where p.party_id=c.party_id) Account, sum(amount) from cash_voucher c \r\n" + 
-				"where party_Id !=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"group by party_Id\r\n" + 
-				") as t1 group by party_Id\r\n" + 
-				"union all\r\n" + 
-				"select concat('P-',LPAD(party_Id, 2, '0')) id,Account,sum(debit),null from (\r\n" + 
-				"SELECT cash_paid_by as party_Id,(select name from party where cash_paid_by=party_id) Account,SUM(amount) debit FROM `general_voucher` where cash_paid_to=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"GROUP BY cash_paid_by\r\n" + 
-				"union all\r\n" + 
-				"select party_Id,(select name from party p where p.party_id=r.party_id) Account,sum(amount) from receipt r \r\n" + 
-				"where party_Id !=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"group by party_Id\r\n" + 
-				") as t2 group by party_id\r\n" + 
-				"union all\r\n" + 
-				"select concat('AC-',LPAD(account_id, 4, '0')),Account,null, sum(credit) from (\r\n" + 
-				"SELECT account_id,(select name from account a where a.account_id=g.account_id) Account,SUM(amount) credit FROM `general_voucher` g where cash_paid_by=1  and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"GROUP BY account_id\r\n" + 
-				"union all\r\n" + 
-				"select account_id,(select name from account a where a.account_id=c.account_id) Account, sum(amount) from cash_voucher c where (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"group by account_id\r\n" + 
-				")as t3 group by account_id\r\n" + 
-				"union all\r\n" + 
-				"select concat('AC-',LPAD(account_id, 4, '0')),Account,sum(debit),null from (\r\n" + 
-				"SELECT account_id,(select name from account a where a.account_id=g.account_id) Account,SUM(amount) as debit, null as Credit FROM `general_voucher` g where cash_paid_to=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"union all\r\n" + 
-				"select account_id,(select name from account a where a.account_id=r.account_id) Account,sum(amount),null from receipt r where (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
-				"group by account_id) as t4\r\n" + 
-				"group by account_id\r\n" + 
-				"order by id desc,Account,debit desc"),
-
+		@NamedNativeQuery(name = "GeneralVoucher.findTrialBalance",
+				query = "select concat('P-',LPAD(party_Id, 2, '0')) id,Account, null as debit,sum(credit) as credit from ( \r\n" + 
+						"select cash_paid_to as party_Id,(select name from party where cash_paid_to=party_id) Account,SUM(amount) credit FROM `general_voucher` where cash_paid_by=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null)  \r\n" + 
+						"group by cash_paid_to\r\n" + 
+						"union all\r\n" + 
+						"select party_Id, (select name from party p where p.party_id=c.party_id) Account, sum(amount) from cash_voucher c \r\n" + 
+						"where party_Id !=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"group by party_Id\r\n" + 
+						") as t1 group by party_Id\r\n" + 
+						"union all\r\n" + 
+						"select concat('P-',LPAD(party_Id, 2, '0')) id,Account,sum(debit),null from (\r\n" + 
+						"SELECT cash_paid_by as party_Id,(select name from party where cash_paid_by=party_id) Account,SUM(amount) debit FROM `general_voucher` where cash_paid_to=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"GROUP BY cash_paid_by\r\n" + 
+						"union all\r\n" + 
+						"select party_Id,(select name from party p where p.party_id=r.party_id) Account,sum(amount) from receipt r \r\n" + 
+						"where party_Id !=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"group by party_Id\r\n" + 
+						") as t2 group by party_id\r\n" + 
+						"union all\r\n" + 
+						"select concat('AC-',LPAD(account_id, 4, '0')),Account,null, sum(credit) from (\r\n" + 
+						"SELECT account_id,(select name from account a where a.account_id=g.account_id) Account,SUM(amount) credit FROM `general_voucher` g where cash_paid_by=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"GROUP BY account_id\r\n" + 
+						"union all\r\n" + 
+						"select account_id,(select name from account a where a.account_id=c.account_id) Account, sum(amount) from cash_voucher c where party_id != 1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"group by account_id\r\n" + 
+						")as t3 group by account_id\r\n" + 
+						"union all\r\n" + 
+						"select concat('AC-',LPAD(account_id, 4, '0')),Account,sum(debit),null from (\r\n" + 
+						"SELECT account_id,(select name from account a where a.account_id=g.account_id) Account,SUM(amount) as debit, null as Credit FROM `general_voucher` g where cash_paid_to=1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"group by account_id\r\n" + 
+						"union all\r\n" + 
+						"select account_id,(select name from account a where a.account_id=r.account_id) Account,sum(amount),null from receipt r where party_id != 1 and (date>=?1 or ?1 is null) and (date<=?2 or ?2 is null) \r\n" + 
+						"group by account_id) as t4\r\n" + 
+						"group by account_id\r\n" + 
+						"order by id desc,Account,debit desc"
+				),
 		@NamedNativeQuery(name = "GeneralVoucher.calculateLedger", query = "select id,date,details,Dr,Cr,(@balance \\:= @balance + (Dr - Cr)) as Balance from \r\n"
 				+ "(SELECT @balance \\:= 0) AS dummy cross join\r\n" + "(\r\n"
 				+ "select concat('JV-',LPAD(general_voucher_id, 4, '0')) id,date,(case when cash_paid_to=1 then amount else '' end) as Dr,(case when cash_paid_by=1 then amount else '' end) as Cr,details from general_voucher where cash_paid_to in (1,?1) and cash_paid_by in (1,?1) and (date>=?2 or ?2 is null) and (date<=?3 or ?3 is null) \r\n"
